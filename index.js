1e7dd1d3a5709829d510dd1e1d39ad24d91bd5c8
Reed Lauber <reedlauber@gmail.com>
2022-05-21 11:59:23 -0700

    Update commander

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const colors = require('colors');
 const program = require('commander');
 
 const commands = require('./lib/commands');
-const importer = require('./lib/import');
 
-module.exports = () => {
+async function run() {
+  let executedCommand;
+
+  const handleCommand = commandName => async cmd => {
+    executedCommand = cmd;
+    await commands[commandName](cmd);
+  }
+
   program
     .version('2.0.0', '--version')
     .usage('[options]');
@@ -13,32 +19,31 @@ module.exports = () => {
   program
     .command('download')
     .description('Download raw agency GTFS files')
-    .option('-a, --agency [agency]', 'Agency slug', /^(septa|trimet)$/)
-    .action(commands.download);
+    .requiredOption('-a, --agency <agency>', 'Agency slug', /^(septa|trimet)$/)
+    .action(handleCommand('download'));
 
   program
     .command('unzip')
     .description('Unzip a downloaded GTFS archive')
-    .option('-a, --agency [agency]', 'Agency slug', /^(septa|trimet)$/)
-    .action(commands.unpack);
+    .requiredOption('-a, --agency <agency>', 'Agency slug', /^(septa|trimet)$/)
+    .action(handleCommand('unpack'));
 
   program
     .command('import')
     .description('Parse and import GTFS data to database')
-    .option('-a, --agency [agency]', 'Agency slug', /^(septa|trimet)$/)
+    .requiredOption('-a, --agency <agency>', 'Agency slug', /^(septa|trimet)$/)
+    .option('-p, --path <path>', 'Unzipped files directory')
     .option('-d, --dry', 'Dry run. Parses files, but does not save to database')
     .option('-t, --type <type>', 'Specific type of data to import (e.g. routes)')
     .option('-v, --verbose', 'Verbose mode')
-    .action(() => {
-      importer.start(program);
-    })
+    .action(handleCommand('import'))
 
   program
     .command('export')
     .description('Export current database to S3')
     .option('-l, --local', 'Create a backup only. Do not export to S3')
     .option('-u, --upload', 'Upload only.')
-    .action(commands.export);
+    .action(handleCommand('export'));
 
   program
     .command('publish')
@@ -47,13 +52,33 @@ module.exports = () => {
     .option('-r, --restore [archive]', 'Restore backup to a production database')
     .option('-p, --promote', 'Promote database to be used by production')
     .option('-x, --delete', 'Remove a production database')
-    .action(commands.publish);
+    .action(handleCommand('publish'));
 
-  program.parse(process.argv);
+  program
+    .command('all')
+    .description('Run download, unzip, and import')
+    .requiredOption('-a, --agency <agency>', 'Agency slug', /^(septa|trimet)$/)
+    .action(async cmd => {
+      executedCommand = cmd;
+      await commands.download(cmd);
+      await commands.unpack(cmd);
+      await commands.import(cmd);
+    });
 
-  if (!program.args.length) {
-    program.help(text => colors.red(text));
-    process.exit(0);
+  try {
+    await program.parseAsync(process.argv);
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
     return;
   }
+
+  if (!executedCommand) {
+    program.help(text => colors.red(text));
+  }
+
+  process.exit(0);
+  return;
 };
+
+module.exports = run;
